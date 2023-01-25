e9e2f68b7a594a7e9a63ff554e8bf9919c7b9be1
StillSlow <89484696+oxiox24@users.noreply.github.com>
2023-01-25 22:22:22 +0000

    added base commands

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-const {Client, Events, GatewayIntentBits} = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const {Client, Collection, Events, GatewayIntentBits} = require('discord.js');
 const {token} = require('./config.json');
 
 const client = new Client({intents: [GatewayIntentBits.Guilds]});
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+}else {
+    console.log(`[WARNING] The command at ${filePath} is missing a 'data' or 'execute' property.`);
+   }
+}
+
+client.on(Events.InteractionCreate, interaction => {
+   if (!interaction.isChatInputCommand()) return;
+    console.log(interaction)
+});
 
 client.once(Events.ClientReady, c => {
 console.log(`Logged in as ${c.user.tag}!`);
